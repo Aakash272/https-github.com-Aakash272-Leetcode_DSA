e679ab8bbbd169112e322d0c09dcacabbe65b440
Aakash Singh <aakashvsingh8@gmail.com>
2026-04-13 09:06:52 +0530

    Time: 4 ms (100%), Space: 49.1 MB (100%) - LeetHub

---
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+import java.util.Arrays;
+
+class Solution {
+    public double[] internalAngles(int[] sides) {
+        Arrays.sort(sides);
+
+        if (sides[0] + sides[1] <= sides[2]) {
+            return new double[]{};
+        }
+
+        double a = sides[0];
+        double b = sides[1];
+        double c = sides[2];
+
+        double angleA = Math.acos((b*b + c*c - a*a) / (2*b*c));
+        double angleB = Math.acos((a*a + c*c - b*b) / (2*a*c));
+        double angleC = Math.acos((a*a + b*b - c*c) / (2*a*b));
+
+        return new double[]{
+            Math.toDegrees(angleA),
+            Math.toDegrees(angleB),
+            Math.toDegrees(angleC)
+        };
+    }
+}

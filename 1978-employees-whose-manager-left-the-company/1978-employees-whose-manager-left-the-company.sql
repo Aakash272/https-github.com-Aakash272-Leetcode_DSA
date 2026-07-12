21825bb58066468f8229f87bd93d7713356ace05
Aakash Singh <aakashvsingh8@gmail.com>
2026-07-13 00:24:35 +0530

    Time: 343 ms (76.15%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,7 @@
+SELECT a.employee_id
+FROM employees a
+LEFT JOIN employees b
+ON a.manager_id = b.employee_id
+WHERE a.salary < 30000
+AND a.manager_id IS NOT NULL
+AND b.employee_id IS NULL order By a.employee_id;

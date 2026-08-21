07e09c95c3b2ef1161e0859b5cf1adf28765ae15
Aakash Singh <aakashvsingh8@gmail.com>
2026-08-21 10:24:00 +0530

    Time: 9 ms (19.27%), Space: 45.3 MB (45.87%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,37 @@
+class Solution {
+        int helper(int arr[],int i,int buy,int dp[][][],int cap){
+     if(cap==0){
+            return 0;
+        } 
+if(i==arr.length){
+    return 0;
+}
+  
+if(dp[i][buy][cap]!=-1){
+    return dp[i][buy][cap];
+}
+if(buy==0){
+    int take=-arr[i]+helper(arr,i+1,1,dp,cap);
+    int nottake=helper(arr,i+1,0,dp,cap);
+   
+   return dp[i][buy][cap]=Math.max(take,nottake);
+}
+else{
+     int take=arr[i]+helper(arr,i+1,0,dp,cap-1);
+    int nottake=helper(arr,i+1,1,dp,cap);
+  return  dp[i][buy][cap]=Math.max(take,nottake); 
+}
+
+}
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+     
+         int dp[][][]=new int[n][2][k+1];
+     for (int i = 0; i < n; i++) {
+            for (int buy = 0; buy < 2; buy++) {
+                Arrays.fill(dp[i][buy], -1);
+            }
+        }
+return helper(prices,0,0,dp,k);
+    }
+}
